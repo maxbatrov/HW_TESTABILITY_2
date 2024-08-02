704a454fff9c9d78be7aedfa85f20a4aefe53280
Max Batrov <maxbatrov@gmail.com>
2024-08-02 15:24:00 +0200

    ReformatCode

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
     public int calculate(int weight, double height) {
-        double result= weight / (height * height);
-        return (int)result;
+        double result = weight / (height * height);
+        return (int) result;
     }
 }
